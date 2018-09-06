fd5633880f54e7f6d1b0e17519136623c6bd6639
colsipple <colsipple@gmail.com>
2018-09-06 15:20:40 -0500

    made it pretty here

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -44,8 +44,7 @@ int isKaprekar(int n) {
     second = square % modulus;
 
     //test if the split makes a Kaprekar number
-    if(second > 0 &&
-       first + second == n) {
+    if(second > 0 && first + second == n) {
       return 1;
     }
   }
